049512babea153f265c6b2dec2111423e989c6df
Wabri <gabriele.puliti@suse.com>
2025-07-24 17:05:14 +0200

    TEMP

--- tests/install/prepare.pm
+++ tests/install/prepare.pm
@@ -1,3 +1,4 @@
+use Mojo::File qw(path);
 use Mojo::Base 'openQAcoretest';
 use testapi;
 use utils qw(login disable_packagekit switch_to_root_console);
@@ -9,9 +10,12 @@ sub run {
     assert_script_run('semanage boolean -m -1 httpd_can_network_connect');
 
     disable_packagekit;
-    assert_script_run('for i in {1..7}; do zypper --no-cd -n in retry && break; sleep $((i**2*20)); done');
+    assert_script_run('for i in {1..7}; do zypper --no-cd -n in retry neofetch && break; sleep $((i**2*20)); done');
     assert_script_run('zypper --no-cd -n rm xscreensaver');
     assert_script_run('pkill -f xscreensaver');
+    assert_script_run("rpm -qa > sut_packages.txt");
+    my $fname = upload_logs("sut_packages.txt");
+    path("ulogs/$fname")->move_to("sut_packages.txt");
 }
 
 1;
